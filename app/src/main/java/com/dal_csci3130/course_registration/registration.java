9f5e9c3c2913f6e0f8a543e245e82442dfa815e6
AJ <al909955@dal.ca>
2018-04-03 22:29:58 -0300

    AJ & Brandon
    Refactored: registration
    
    Result: Added and removed spacing regarding methods that did not follow consistency of the project.

--- app/src/main/java/com/dal_csci3130/course_registration/registration.java
+++ app/src/main/java/com/dal_csci3130/course_registration/registration.java
@@ -8,7 +8,6 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
