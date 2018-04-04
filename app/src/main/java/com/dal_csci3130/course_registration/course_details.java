33ecb32d34d8c3d40f5c0c613cd556ed69cc8230
AJ <al909955@dal.ca>
2018-04-03 22:00:20 -0300

    AJ & Ryan Refactored: course_details class
    Result: Changed indentations of code in several methods, matched spacing through the methods for consistency.

--- app/src/main/java/com/dal_csci3130/course_registration/course_details.java
+++ app/src/main/java/com/dal_csci3130/course_registration/course_details.java
@@ -12,7 +12,6 @@ import java.util.Objects;
 
 public class course_details extends AppCompatActivity {
 
-
     private TextView tview;
     private TextView notice;
     private Course course;
@@ -23,111 +22,97 @@ public class course_details extends AppCompatActivity {
     private DataBase db;
     private int rating;
 
-
     @SuppressLint("SetTextI18n")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_course_details);
-
         Intent i = getIntent();
         Bundle extras = i.getExtras();
         user = (User) extras.getSerializable("user");
         course = (Course) extras.getSerializable("course");
         db = (DataBase) extras.getSerializable("database");
-
         completed = user.getCompleted();
         current = user.getCurrent();
         remaining = user.getRemaining();
         rating = 0;
         int max = 0;
-
-        if (course.getRating()!=null) {
-            System.out.println("rating size : "+course.getRating().size());
+        if (course.getRating() != null) {
+            System.out.println("rating size : " + course.getRating().size());
             for (int x = 0; x < course.getRating().size(); x++) {
-                System.out.println("rating : "+course.getRating().get(x));
+                System.out.println("rating : " + course.getRating().get(x));
                 rating += course.getRating().get(x);
-                max+=1;
+                max += 1;
             }
-        };
-
+        }
+        ;
         notice = (TextView) findViewById(R.id.notice);
         tview = (TextView) findViewById(R.id.textView);
-        tview.setText(course.getTitle()+course.getYear()+
-                "\nRemaining:"+course.getRem()+
-                "\nCapacity:"+course.getCap()+
-                "\nDays:"+course.getDays()+
-                "\nTime:"+course.getTime()+
-                "\nTerm:"+course.getTerm()+
-                "\nProffessor:"+course.getProfessor()+
-                "\nRating: "+(rating/1.0)/max+
-                "\nBuilding:"+course.getLocation()+
-                "\nDescription:\n"+course.getDescription()+
-                "\n\nPrereq of: "+course.getPrereq()+
-                "\n\nPrereq for: "+course.getPrereqf());
-
+        tview.setText(course.getTitle() + course.getYear() +
+                "\nRemaining:" + course.getRem() +
+                "\nCapacity:" + course.getCap() +
+                "\nDays:" + course.getDays() +
+                "\nTime:" + course.getTime() +
+                "\nTerm:" + course.getTerm() +
+                "\nProffessor:" + course.getProfessor() +
+                "\nRating: " + (rating / 1.0) / max +
+                "\nBuilding:" + course.getLocation() +
+                "\nDescription:\n" + course.getDescription() +
+                "\n\nPrereq of: " + course.getPrereq() +
+                "\n\nPrereq for: " + course.getPrereqf());
     }
 
     @SuppressLint("SetTextI18n")
     public void addClass(View view) {
-
         boolean problem = false;
         boolean timeconflict = false;
         Course conflict1;
         Course conflict2;
-
-        for (int x=0; x<completed.size();x++) {
+        for (int x = 0; x < completed.size(); x++) {
             if (course.getFaculty().equals(completed.get(x).getFaculty())) {
                 if (course.getYear().equals(completed.get(x).getYear())) {
                     System.out.println("Already Completed");
                     notice.setText("Already Completed\n");
                     problem = true;
                 }
-            }
-            else {
+            } else {
                 notice.setText("");
             }
         }
-
         //check if course registered already
-        for (int x = 0; x< current.size(); x++) {
+        for (int x = 0; x < current.size(); x++) {
             if (Objects.equals(course.getFaculty(), current.get(x).getFaculty()) && !problem) {
                 if (Objects.equals(course.getYear(), current.get(x).getYear())) {
                     System.out.println("Already signed up\n");
-                    notice.setText(notice.getText()+"Already Registered\n");
+                    notice.setText(notice.getText() + "Already Registered\n");
                     problem = true;
                 }
             }
         }
-
         //check if course full
         if (Integer.parseInt(course.getRem()) < 1 && !problem) {
             System.out.println("course full");
-            notice.setText(notice.getText()+"Course is full");
+            notice.setText(notice.getText() + "Course is full");
             problem = true;
         }
-
         //check time conflict
         if (!problem) {
-            for (int x=1; x<current.size();x++) {
+            for (int x = 1; x < current.size(); x++) {
                 if (current.get(x).getTime() == course.getTime()) {
                     if (current.get(x).getDays() == course.getDays()) {
                         //todo: check conflict conditions for edge cases
                         System.out.println("Time conflict between two classes\n");
-                        notice.setText(notice.getText()+"Time conflict between two courses");
+                        notice.setText(notice.getText() + "Time conflict between two courses");
                         problem = true;
 
                     }
                 }
             }
         }
-
         //if problem
         if (!problem) {
-
-
             System.out.println("rem_before: " + course.getRem());
-            course.setRem(""+(Integer.parseInt(course.getRem())-1));
+            course.setRem("" + (Integer.parseInt(course.getRem()) - 1));
             System.out.println("rem_after: " + course.getRem());
 
             notice.setText("Course Successfully Added!");
@@ -136,8 +121,6 @@ public class course_details extends AppCompatActivity {
             user.setCurrent(current);
             user.setRemaining(remaining);
             db.updateCourse(course);
-
-
         }
     }
 
